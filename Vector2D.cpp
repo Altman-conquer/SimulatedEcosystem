f3a499961c24d825a33ffb902edb82688667b00b
王侯 <wanghou_2411@qq.com>
2023-03-04 15:03:44 +0800

    add GetDistance

--- Vector2D.cpp
+++ Vector2D.cpp
@@ -1,5 +1,6 @@
 #include "Vector2D.h"
 #include "Log.h"
+//#include <math.h>
 
 Vector2D::Vector2D() :Vector2D(0.0, 0.0) {}
 
@@ -21,6 +22,21 @@ Vector2D Vector2D::GetDirectionVector(Direction direction)
 		Log::LogMessage("In function Vector2D::GetDirectionVector, direction is out of bounds", LogLevel::Error);
 		return Vector2D(0.0, 0.0);
 	}
+}
+
+float Vector2D::GetDistance(const Vector2D& vector1, const Vector2D& vector2)
+{
+	return sqrt(pow(vector1.x - vector2.x,2) + pow(vector1.y - vector2.y, 2));
+}
+
+float Vector2D::GetY()
+{
+	return y;
+}
+
+float Vector2D::GetX()
+{
+	return x;
 }
 
 Vector2D Vector2D::operator+(const Vector2D& other) const
