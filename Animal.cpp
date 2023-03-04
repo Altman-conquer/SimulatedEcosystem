0df74b8e82e71fe14da65245befcd67a56f6f1de
xuan gan <1320007169@qq.com>
2023-03-04 10:45:21 +0800

    123

--- Animal.cpp
+++ Animal.cpp
@@ -5,6 +5,7 @@ Animal::Animal(std::shared_ptr<vector<shared_ptr<Animal>>> _environment)
 	this->environment = _environment;
 }
 //12332
+//sadsad
 Vector2D Animal::GetPosition()
 {
 	return position;
